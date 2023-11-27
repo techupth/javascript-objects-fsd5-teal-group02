8413b88c8f774dec00127ce7728ebe53cb49810e
Palm <palml1lax@gmail.com>
2023-11-27 10:36:51 +0700

    fix : fix ex-3

--- ex-3.js
+++ ex-3.js
@@ -27,14 +27,3 @@ console.log(
     " " +
     employee.address.postalCode
 );
-
-const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-
-let text = "";
-for (let i = 0; i < cars.length; i++) {
-  if (i == 3) {
-    continue;
-  }
-  text += cars[i] + "<br>";
-}
-console.log(text);
